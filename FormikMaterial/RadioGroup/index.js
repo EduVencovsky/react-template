4664c95ec4eade59c68d4c795199d3b920030b42
Eduardo Vencovsky <eduardo.vencovsky@gatec.com.br>
2020-01-27 08:40:07 -0300

    CheckBox added

--- FormikMaterial/RadioGroup/index.js
+++ FormikMaterial/RadioGroup/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import { Field } from 'formik'
 
 const MaterialRadioGroup = ({ formik, name, options, children, label, ...otherProps }) => (
     <RadioGroup
