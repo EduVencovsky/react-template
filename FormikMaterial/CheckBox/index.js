a084c88054d39a22095c8ed10febd880cc9efd35
Eduardo Vencovsky <eduardo.vencovsky@gatec.com.br>
2020-01-27 08:40:41 -0300

    CheckBox added

---
+++ FormikMaterial/CheckBox/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import { Field } from 'formik'
+
+export const MaterialCheckBox = ({ field, form, disabled, ...props }) => {
+    const { isSubmitting } = form;
+
+    return (
+        <FormControlLabel
+            {...field}
+            {...props}
+            disabled={disabled != undefined ? disabled : isSubmitting}
+            control={<Checkbox />}
+        />
+    )
+}
+
+MaterialCheckBox.defaultProps = {
+    label: '',
+}
+
+MaterialCheckBox.propTypes = {
+    disabled: PropTypes.bool,
+    form: PropTypes.object.isRequired,
+    field: PropTypes.object.isRequired,
+}
+
+const MUIFormikCheckBox = props => <Field {...props} component={MaterialCheckBox} />
+
+MUIFormikCheckBox.propTypes = {
+    name: PropTypes.string.isRequired
+}
+
+export default MUIFormikCheckBox
